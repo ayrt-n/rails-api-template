e06b3b534daca404e2b9f148d3cbbbe7d0cff494
ayrton <ayrton.parkinson1@gmail.com>
2023-01-14 14:27:59 -0500

    Fix hanging comma error

--- application_template.rb
+++ application_template.rb
@@ -10,7 +10,7 @@ initializer 'generators.rb', <<~CODE
   Rails.application.config.generators do |g|
     g.test_framework :rspec,
       view_specs:       false,
-      controller_specs: false,
+      controller_specs: false
   end
 CODE
 
