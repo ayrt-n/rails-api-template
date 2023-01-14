f4a64b5c7b8362e347e7e071472ba92847517a80
ayrton <ayrton.parkinson1@gmail.com>
2023-01-14 14:16:04 -0500

    Set up rspec config

--- application_template.rb
+++ application_template.rb
@@ -9,10 +9,7 @@ end
 initializer 'generators.rb', <<~CODE
   Rails.application.config.generators do |g|
     g.test_framework :rspec,
-      fixtures:         false,
-      view_specs:       false
-      helper_specs:     false,
-      routing_specs:    false,
+      view_specs:       false,
       controller_specs: false,
   end
 CODE
